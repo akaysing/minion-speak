fa52c812e39c43c52bca027bedb5caa2df0e1cc6
Adarsh Kumar Singh <adarshk03.singh@gmail.com>
2021-10-02 11:11:22 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const buttonPrimary = document.querySelector("#btn-primary");
+const inputText = document.querySelector("#txt-area");
+const outputText = document.querySelector("#txt-output");
+const serverUrl = "https://api.funtranslations.com/translate/minion.json";
+var fetchUrl;
+
+function translationUrl() {
+    fetchUrl = serverUrl + "?" + "text=" + inputText.value;
+}
+
+buttonPrimary.addEventListener("click", function () {
+    translationUrl();
+    fetch(fetchUrl)
+        .then(response => response.json())
+        .then(json => outputText.innerText = json.contents.translated)
+});
