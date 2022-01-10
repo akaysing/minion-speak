fe3c09c535aef3f2fe363159f9381ee31189469b
Adarsh Kumar Singh <adarshk03.singh@gmail.com>
2022-01-10 23:39:03 +0530

    ES6+ update--used arrow functions

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ const buttonPrimary = document.querySelector("#btn-primary");
 const inputText = document.querySelector("#txt-area");
 const outputText = document.querySelector("#txt-output");
 const serverUrl = "https://api.funtranslations.com/translate/minion.json";
-var fetchUrl;
+let fetchUrl;
 
-function translationUrl() {
-    fetchUrl = serverUrl + "?" + "text=" + inputText.value;
-}
+const translationUrl = () => fetchUrl = serverUrl + "?" + "text=" + inputText.value;
 
-buttonPrimary.addEventListener("click", function () {
+buttonPrimary.addEventListener("click", () => {
     translationUrl();
     fetch(fetchUrl)
         .then(response => response.json())
         .then(json => outputText.innerText = json.contents.translated)
-});
+
+})
